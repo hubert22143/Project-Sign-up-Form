70b8fbab77a3475a8e0a793a683fd5f38f9947b7
Hubert Baciak <hubert.baciak@o2.pl>
2023-09-03 14:37:22 +0200

    Added several inputs rules, working on form submitting

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const emailInput = document.getElementById("email");
 const phoneInput = document.getElementById("phone-number");
 const passwordInput = document.getElementById("password");
 const submitPasswordInput = document.getElementById("submit-password");
-
+const inputElements = document.querySelectorAll('input[id]');
+const cryteriaDivs = document.querySelectorAll('.cryteria');
 const criteriaDivs = {
     'first-name': document.getElementById('thename'),
     'last-name': document.getElementById('thelastname'),
@@ -15,7 +16,68 @@ const criteriaDivs = {
     'submit-password': document.getElementById('thesubmitpassword')
 };
 
+const patterns = {
+    'first-name': /^[A-Za-z]{4,}$/,
+    'last-name': /^[A-Za-z]+$/,
+    'email': /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
+    'phone-number': /^\+\(\d{2}\)\d{9}$/,
+    'password': /^.{6,}$/,
+    'submit-password': /^.{6,}$/ 
+};
+
+const inputIds = [
+    'first-name',
+    'last-name',
+    'email',
+    'phone-number',
+    'password',
+    'submit-password'
+];
+inputElements.forEach((inputElement) => {
+    let timeoutId; // Variable to store the timeout ID
+
+    inputElement.addEventListener('input', () => {
+        const inputId = inputElement.getAttribute('id');
+        const associatedDiv = document.querySelector(`.cryteria[data-input-id="${inputId}"]`);
+
+        // Clear any previous timeouts
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // Set a timeout to apply the class changes after a brief delay (e.g., 300 milliseconds)
+        timeoutId = setTimeout(() => {
+            if (associatedDiv) {
+                if (inputElement.classList.contains('valid')) {
+                    associatedDiv.classList.add('valid');
+                    associatedDiv.classList.remove('invalid');
+                } else {
+                    associatedDiv.classList.remove('valid');
+                    associatedDiv.classList.add('invalid');
+                }
+            }
+        }, 300); // Adjust the delay as needed
+    });
+});
+inputIds.forEach(idInput => {
+    const inputElement = document.getElementById(idInput);
+    inputElement.addEventListener('input', () => {
+        validateInput(inputElement,idInput);
+    })
+})
+function validateInput(inputElement,patternKey){
+    const pattern = patterns[patternKey];
+    const inputValue = inputElement.value;
+    if(pattern.test(inputValue)){
+        inputElement.classList.add('valid');
+        inputElement.classList.remove('invalid')
+    }else{
+        inputElement.classList.add('invalid');
+        inputElement.classList.remove('valid');
+    }
+}
+
 const inputsArray = [nameInput,surnameInput,emailInput,phoneInput,passwordInput,submitPasswordInput];
+
 inputsArray.forEach((input) => {
     input.addEventListener('click', () => {
         const inputId = input.id;
@@ -23,10 +85,27 @@ inputsArray.forEach((input) => {
             criteriaDivs[key].style.visibility = 'hidden';
         }
 
-        // Show the corresponding criteria div
         if (criteriaDivs[inputId]) {
             criteriaDivs[inputId].style.visibility = "visible";
         }
     });
 });
+function passwordsMatch() {
+    const passwordInput = document.getElementById("password");
+    const submitPasswordInput = document.getElementById("submit-password");
+
+    // Get the values of the password and submit-password inputs
+    const passwordValue = passwordInput.value;
+    const submitPasswordValue = submitPasswordInput.value;
+
+    // Compare the values and return true if they match, false otherwise
+    return passwordValue === submitPasswordValue;
+}
+document.querySelector("form").addEventListener("submit", function (event) {
+    if (!passwordsMatch()) {
+        event.preventDefault(); // Prevent form submission if passwords don't match
+        alert("Passwords do not match. Please re-enter them.");
+    }
+});
+
 });
